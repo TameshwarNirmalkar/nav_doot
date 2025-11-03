01d556a957d39ad95b912f5c524bee572fd5c016
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2025-11-03 22:20:58 +0530

    remove tailwind config

--- tailwind.config copy.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // ... other configs
-  corePlugins: {
-    preflight: false, // <-- Disable Tailwind's base reset styles
-  },
-  // ...
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+  corePlugins: {
+    preflight: false, // <-- Disable Tailwind's base reset styles
+  },
+};
