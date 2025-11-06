797dc6b47e86b6797d66ea35ea203211977847bb
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2025-11-07 00:27:01 +0530

    tab index form

--- src/app/(authorized)/location/partners/_components/AddPartnerForm.tsx
+++ src/app/(authorized)/location/partners/_components/AddPartnerForm.tsx
@@ -8,7 +8,7 @@ import { selectCountryCityList } from '@src/store/country_cities';
 import { getAllCitiesBasedOnCountryAndStateAction, getAllStatesBasedOnCountryAction } from '@src/store/country_cities/action';
 import { selectAllCities, selectAllState } from '@src/store/country_cities/memonised_country_city_selector';
 import { useAppDispatch, useAppSelector } from '@src/store/redux_hooks';
-import { Form, FormInstance, Input, Select } from 'antd';
+import { Divider, Form, FormInstance, Input, Select } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import React, { memo, useCallback } from 'react';
 
@@ -66,81 +66,114 @@ const AddCustomerForm = () => {
   );
 
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <div>
-        <Form.Item name="id" hidden>
-          <Input hidden />
-        </Form.Item>
-        <Form.Item name="state_name" hidden>
-          <Input hidden />
-        </Form.Item>
-        <Form.Item name="city_name" hidden>
-          <Input hidden />
-        </Form.Item>
-        <Form.Item name="branch_type" hidden>
-          <Input hidden />
-        </Form.Item>
-        <Form.Item name="parent_branch_name" hidden>
-          <Input hidden />
-        </Form.Item>
+    <>
+      <Divider size="small" orientation="right">
+        <span className="text-gray-500">General Detail</span>
+      </Divider>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <Form.Item name="id" hidden>
+            <Input hidden />
+          </Form.Item>
+          <Form.Item name="state_name" hidden>
+            <Input hidden />
+          </Form.Item>
+          <Form.Item name="city_name" hidden>
+            <Input hidden />
+          </Form.Item>
+          <Form.Item name="branch_type" hidden>
+            <Input hidden />
+          </Form.Item>
+          <Form.Item name="parent_branch_name" hidden>
+            <Input hidden />
+          </Form.Item>
 
-        <Form.Item label="Full Name" name="customer_name" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter Name" />
-        </Form.Item>
+          <Form.Item label="Full Name" name="customer_name" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter Name" tabIndex={1} />
+          </Form.Item>
 
-        <Form.Item label="Pan Number" name="pan_number" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter Pan Number" />
-        </Form.Item>
-
-        <SelectWithAdd dropDownList={branchTypeList.map((el) => ({ field_name: el.branchtype_name, field_id: el.branchtype_id }))} loadingState={isBranchLoading} field_id="branchtype_id" formItemLabel="Branch Type" buttonLabel="Add" onAddHandler={onAddBranch} onItemSelectHandler={onBranchTypeSelect} />
-
-        <SelectWithAdd dropDownList={branchTypeList.map((el) => ({ field_name: el.branchtype_name, field_id: el.branchtype_id }))} loadingState={isBranchLoading} field_id="parent_branch_code" formItemLabel="Parent Branch" buttonLabel="Add" onAddHandler={onAddBranch} onItemSelectHandler={onParentBranchTypeSelect} />
-        <Form.Item label="GST Number" name="gst_number" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter GST Number" />
-        </Form.Item>
-        <Form.Item label="Contact Person" name="contact_person" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter Contact Person" />
-        </Form.Item>
-        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter Email" />
-        </Form.Item>
-      </div>
-      <div>
-        <Form.Item label="Phone" name="phone" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter Phone Number" />
-        </Form.Item>
-        <Form.Item label="Alternate Phone" name="alternate_phone">
-          <Input placeholder="Enter Alternate Phone Number" />
-        </Form.Item>
-        <Form.Item label="Allow Scan" name="allow_scan" rules={[{ required: true, message: 'Required' }]}>
-          <Select
-            placeholder="Select Allow Scan"
-            options={[
-              { label: 'Yes', value: 'Y' },
-              { label: 'No', value: 'N' },
-            ]}
+          <SelectWithAdd
+            dropDownList={branchTypeList.map((el) => ({ field_name: el.branchtype_name, field_id: el.branchtype_id }))}
+            loadingState={isBranchLoading}
+            field_id="branchtype_id"
+            formItemLabel="Branch Type"
+            buttonLabel="Add"
+            onAddHandler={onAddBranch}
+            onItemSelectHandler={onBranchTypeSelect}
+            htmlProps={{ tabIndex: 4 }}
           />
-        </Form.Item>
-        <Form.Item label="Postal Code" name="postal_code" rules={[{ required: true, message: 'Required' }]}>
-          <Input placeholder="Enter postal code." />
-        </Form.Item>
-        <Form.Item label="City" name="city_code" rules={[{ required: true, message: 'Required' }]}>
-          <Select showSearch placeholder="Select City" filterSort={(optionA, optionB) => (optionA.name ?? '').toLowerCase().localeCompare((optionB.name ?? '').toLowerCase())} options={allCities} optionFilterProp="name" fieldNames={{ label: 'name', value: 'id' }} onSelect={onCitySelect} />
-        </Form.Item>
 
-        <Form.Item label="State" name="state_code" rules={[{ required: true, message: 'Required' }]}>
-          <Select showSearch placeholder="Select State" filterSort={(optionA, optionB) => (optionA.name ?? '').toLowerCase().localeCompare((optionB.name ?? '').toLowerCase())} fieldNames={{ label: 'name', value: 'id' }} optionFilterProp="name" options={allStates} onSelect={onStateSelect} />
-        </Form.Item>
+          <Form.Item label="GST Number" name="gst_number" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter GST Number" tabIndex={5} />
+          </Form.Item>
+        </div>
+        <div>
+          <Form.Item label="Pan Number" name="pan_number" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter Pan Number" tabIndex={2} />
+          </Form.Item>
 
-        <Form.Item label="Country" name="country_code" rules={[{ required: true, message: 'Required' }]}>
-          <Select showSearch placeholder="Select Country" optionFilterProp="name" filterSort={(optionA, optionB) => (optionA?.name ?? '').toLowerCase().localeCompare((optionB?.name ?? '').toLowerCase())} fieldNames={{ label: 'name', value: 'id' }} options={allCountries} onSelect={onCountrySelect} />
-        </Form.Item>
+          <SelectWithAdd
+            dropDownList={branchTypeList.map((el) => ({ field_name: el.branchtype_name, field_id: el.branchtype_id }))}
+            loadingState={isBranchLoading}
+            field_id="parent_branch_code"
+            formItemLabel="Parent Branch"
+            buttonLabel="Add"
+            onAddHandler={onAddBranch}
+            onItemSelectHandler={onParentBranchTypeSelect}
+            htmlProps={{ tabIndex: 4 }}
+          />
 
-        <Form.Item label="Address" name="address" rules={[{ required: true, message: 'Required' }]}>
-          <TextArea rows={5} cols={6} placeholder="Enter Address" />
-        </Form.Item>
+          <Form.Item label="Allow Scan" name="allow_scan" rules={[{ required: true, message: 'Required' }]}>
+            <Select
+              placeholder="Select Allow Scan"
+              options={[
+                { label: 'Yes', value: 'Y' },
+                { label: 'No', value: 'N' },
+              ]}
+              tabIndex={6}
+            />
+          </Form.Item>
+        </div>
       </div>
-    </div>
+      <Divider size="small" orientation="right">
+        <span className="text-gray-500">Contact Details</span>
+      </Divider>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <Form.Item label="Contact Person" name="contact_person" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter Contact Person" tabIndex={7} />
+          </Form.Item>
+          <Form.Item label="Phone" name="phone" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter Phone Number" tabIndex={9} />
+          </Form.Item>
+
+          <Form.Item label="Postal Code" name="postal_code" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter postal code." tabIndex={11} />
+          </Form.Item>
+
+          <Form.Item label="State" name="state_code" rules={[{ required: true, message: 'Required' }]}>
+            <Select tabIndex={13} showSearch placeholder="Select State" filterSort={(optionA, optionB) => (optionA.name ?? '').toLowerCase().localeCompare((optionB.name ?? '').toLowerCase())} fieldNames={{ label: 'name', value: 'id' }} optionFilterProp="name" options={allStates} onSelect={onStateSelect} />
+          </Form.Item>
+        </div>
+        <div>
+          <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Required' }]}>
+            <Input placeholder="Enter Email" tabIndex={8} />
+          </Form.Item>
+          <Form.Item label="Alternate Phone" name="alternate_phone">
+            <Input placeholder="Enter Alternate Phone Number" tabIndex={10} />
+          </Form.Item>
+          <Form.Item label="City" name="city_code" rules={[{ required: true, message: 'Required' }]}>
+            <Select tabIndex={12} showSearch placeholder="Select City" filterSort={(optionA, optionB) => (optionA.name ?? '').toLowerCase().localeCompare((optionB.name ?? '').toLowerCase())} options={allCities} optionFilterProp="name" fieldNames={{ label: 'name', value: 'id' }} onSelect={onCitySelect} />
+          </Form.Item>
+          <Form.Item label="Country" name="country_code" rules={[{ required: true, message: 'Required' }]}>
+            <Select tabIndex={14} showSearch placeholder="Select Country" optionFilterProp="name" filterSort={(optionA, optionB) => (optionA?.name ?? '').toLowerCase().localeCompare((optionB?.name ?? '').toLowerCase())} fieldNames={{ label: 'name', value: 'id' }} options={allCountries} onSelect={onCountrySelect} />
+          </Form.Item>
+        </div>
+      </div>
+      <Form.Item label="Address" name="address" rules={[{ required: true, message: 'Required' }]}>
+        <TextArea rows={2} cols={2} placeholder="Enter Address" tabIndex={15} />
+      </Form.Item>
+    </>
   );
 };
 
